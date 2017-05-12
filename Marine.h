3410b89a049fde4dcc31ba565cbaf21f19e71242
ChrizzFeller <chrizzhenriquez@unitec.edu>
2017-05-12 13:47:45 -0600

    Clases llenadas.

---
+++ Marine.h
@@ -0,0 +1,24 @@
+#include <vector>
+#include "Pieza.h"
+
+#ifndef MARINE_H
+#define MARINE_H
+
+using namespace std;
+
+class Marine
+{
+	private:
+		
+	public:
+		Marine();
+		Marine(int, int);
+
+		int getX();
+		void setX(int);
+		int getY();
+		void setY(int);
+		
+	
+};
+#endif
